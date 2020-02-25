ecbb2aca5c8dac8f2fe63e1a9b6fa40e0380882d
Nicolas Fonsat <nfonsat@witekio.com>
2020-02-25 01:13:51 +0100

    Implement Book Cart in Detail View

--- HenriPotier/Views/BookDetailViewController.swift
+++ HenriPotier/Views/BookDetailViewController.swift
@@ -20,8 +20,12 @@ class BookDetailViewController: UIViewController {
     
     @IBOutlet var basketButton: UIButton!
     
+    private var cartService: BookCartService {
+        return BookCartService.shared
+    }
+    
     var book: BookModel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.bookTitleLabel.text = self.book.title
@@ -32,8 +36,24 @@ class BookDetailViewController: UIViewController {
         self.bookSynopsisTextView.text = sinopsis
         
         self.basketButton.layer.cornerRadius = self.basketButton.frame.height * 0.5
-
+        self.updateCartButton()
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func addCartAction(_ sender: UIButton) {
+        if self.cartService.containsBook(self.book) {
+            self.cartService.removeBookItem(self.book)
+        } else {
+            self.cartService.addBookItem(self.book)
+        }
+        
+        self.updateCartButton()
+    }
+    
+    func updateCartButton() {
+        let contains = self.cartService.containsBook(self.book)
+        self.basketButton.setTitle(contains ? "-" : "+", for: .normal)
+    }
+    
 }
