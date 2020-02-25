940ceacdab521dc101a9bfedde1a7425d6d22a87
Nicolas Fonsat <nfonsat@witekio.com>
2020-02-26 00:15:39 +0100

    added CartView Controller

---
+++ HenriPotier/Views/CartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CartViewController.swift
+//  HenriPotier
+//
+//  Created by Nicolas Fonsat on 26/02/2020.
+//  Copyright © 2020 BlackStyle. All rights reserved.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
